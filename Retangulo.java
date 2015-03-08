da3fba80dcfeb0ca7d8dc7d836d428d3ed220871
VanessaSassi <burnoutvanessa@gmail.com>
2015-03-08 18:06:02 -0300

    Classe retangulo e calculos

--- Retangulo.java
+++ Retangulo.java
@@ -5,4 +5,7 @@ class Retangulo{
 	public void area(){
 		System.out.println("A area do retangulo e: " this.base * this.altura);
 	}
+	public void perimetro(){
+		System.out.println("O perimetro do retangulo e: " 2*(this.base + this.altura);
+	}
 }  
