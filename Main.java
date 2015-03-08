a1f7b90fb4670d3b1c811800262f45c783a8f2ee
VanessaSassi <burnoutvanessa@gmail.com>
2015-03-08 18:45:49 -0300

    Classe main com valores de teste para a altura e base

---
+++ Main.java
@@ -0,0 +1,10 @@
+class Main{
+	public static void main (String args[]{
+		Retangulo = retangulo;
+		retangulo = new Retangulo;
+		retangulo.base = 3.14;
+		retangulo.altura = 7;
+		retangulo.area();
+		retangulo.perimetro();
+	}
+}
