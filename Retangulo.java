47de5c109737a70eb3a783ff4ca06df4f6bd4ee4
VanessaSassi <burnoutvanessa@gmail.com>
2015-03-08 19:05:53 -0300

    Classe Retangulo com atributos e calculos

--- Retangulo.java
+++ Retangulo.java
@@ -1,11 +1,11 @@
-class Retangulo{
+public class Retangulo{
 	public double base;
 	public double altura;
 	
 	public void area(){
-		System.out.println("A area do retangulo e: " this.base * this.altura);
+		System.out.println("A area do retangulo e: "+ this.base * this.altura);
 	}
 	public void perimetro(){
-		System.out.println("O perimetro do retangulo e: " 2*(this.base + this.altura);
+		System.out.println("O perimetro do retangulo e: "+ 2*(this.base + this.altura));
 	}
 }  
